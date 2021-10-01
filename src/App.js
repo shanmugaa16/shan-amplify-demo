d2dc11238b5ff52471dfb86ef3c12327c43c4f2f
shanmugaa16 <shanmugaa16@gmail.com>
2021-10-01 11:43:25 -0500

    updated with AWS

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Shan React page!!
+          Welcome to Shan React page with AWS Amplify!!
         </p>
         
       </header>
